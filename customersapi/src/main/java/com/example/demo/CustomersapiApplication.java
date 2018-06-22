1d8826565bc8f4ff2d97c9683b1da99bbd94a928
DESKTOP-GQJF4JT\Bipin Singh <bipinsingh0225@gmail.com>
2018-06-22 23:15:00 +0530

    commit 1

--- customersapi/src/main/java/com/example/demo/CustomersapiApplication.java
+++ customersapi/src/main/java/com/example/demo/CustomersapiApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class CustomersapiApplication {
 
+	xzczx
+
 	public static void main(String[] args) {
 		SpringApplication.run(CustomersapiApplication.class, args);
 	}
